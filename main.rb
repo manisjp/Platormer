9d8661f92b8762ded5a8b90cbdb0785e8f41b638
Josh Manis <manisjp@s.dcsdk12.org>
2015-12-07 20:46:03 -0700

    Refactoring

--- main.rb
+++ main.rb
@@ -26,37 +26,27 @@ class GameWindow < Gosu::Window
 		@platforms = []
 		@fishes = []
 
-		# create the starting platform
-		@platforms.push(Platform.new(width/2 - 125, 950))
+		create_start_platform
 	end
 
 	def update
-		# death condition
 		return if game_over?
 
-		# create platforms and fish and push them into arrays
-		(1...1000).each do |i|
-			@platforms.push(Platform.new(rand*475, i*200 + 100)) if @platforms.length < 5
-		end
-		@fishes.push(Fish.new) if @fishes.size < 10
-
-		@platforms.reject! { |platform| platform.y > 1000}
-		@fishes.reject! { |fish| fish.y > 1000}
-
-		# allow player to collect fish and count score
-		@player.eat(@fishes)
-
-		# player jumps when on platform no matter what
-		@player.jump if @player.standing?(@platforms)
-		# user input using arrow keys
-		@player.left if Gosu::button_down?(Gosu::KbLeft)
-		@player.right if Gosu::button_down?(Gosu::KbRight)
-
-		# allow player to move
-		@player.move
-		# accelerate player down if not standing
 		gravity
-		scroll if @player.y < 500
+		control_player
+		
+		create_objects
+		remove_offscreen(@platforms)
+		remove_offscreen(@fishes)
+
+		@player.eat(@fishes)
+		@player.jump if @player.standing?(@platforms)
+		@player.move
+
+		if @player.y < self.height/2
+			scroll(@platforms)
+			scroll(@fishes)
+		end
 	end
 
 	def draw
@@ -79,32 +69,49 @@ class GameWindow < Gosu::Window
 		@fishes.each { |fish| fish.draw }
 	end
 
-	# buttons if pressed anywhere
-	def button_down id
-		close if id == Gosu::KbEscape
-	end
-
-	# if standing downwards acceleration stops
-	def gravity
-		@player.standing?(@platforms) ? @player.vel_y = 0 : @player.vel_y -= GRAVITY
-	end
-
-	# handles the scrolling of the platforms and the fish
-	def scroll
-		@platforms.each do |platform|
-			platform.y += @player.vel_y if @player.vel_y > 0
-			platform.hit_box.y += @player.vel_y if @player.vel_y > 0
+	private
+		# buttons if pressed anywhere
+		def button_down id
+			close if id == Gosu::KbEscape
 		end
-		@fishes.each do |fish|
-			fish.y += @player.vel_y if @player.vel_y > 0
-			fish.hit_box.y += @player.vel_y if @player.vel_y > 0
-		end
-	end
 
-	# used to display the end screen and end game
-	def game_over?
-		true if @player.y > 990
-	end
+		# if standing downwards acceleration stops
+		def gravity
+			@player.standing?(@platforms) ? @player.vel_y = 0 : @player.vel_y -= GRAVITY
+		end
+
+		# handles the scrolling of the platforms and the fish
+		def scroll objects
+			objects.each do |object|
+				object.y += @player.vel_y if @player.vel_y > 0
+				object.hit_box.y += @player.vel_y if @player.vel_y > 0
+			end
+		end
+
+		# used to display the end screen and end game
+		def game_over?
+			true if @player.y > self.height - 5
+		end
+
+		def create_start_platform
+			@platforms.push(Platform.new(width/2 - 125, 950))
+		end
+
+		def create_objects
+			(0..3).each do |i|
+				@platforms.push(Platform.new(rand*475, i*200 + 300)) if @platforms.length < 5
+			end
+			@fishes.push(Fish.new) if @fishes.size < 10
+		end
+
+		def control_player
+			@player.left if Gosu::button_down? Gosu::KbLeft
+			@player.right if Gosu::button_down? Gosu::KbRight
+		end
+
+		def remove_offscreen objects
+			objects.reject! { |object| object.y > 1000}
+		end
 
 end
 
